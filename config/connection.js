5b3884ba17852942ddcc36e5498c00a12209e27d
mhans003 <michaeledwardhanson@gmail.com>
2020-10-31 09:09:18 -0700

    Changed mysql connection configuration for Heroku.

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,15 @@ const mysql = require("mysql");
 
 //Define connection. 
 const connection = mysql.createConnection({
-  host: "localhost",
+  //host: "localhost",
+  host: "us-cdbr-east-02.cleardb.com",
   port: 3306,
-  user: "root",
-  password: "",
-  database: "todolist_db"
+  //user: "root",
+  user: "bc63f9baf3dc33",
+  //password: "",
+  password: "b8ceb667",
+  //database: "todolist_db"
+  database: "heroku_46a814c5751ed29"
 });
 
 //Establish connection. 
