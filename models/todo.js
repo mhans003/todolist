31c18a41d3ab7b75785dc1a73762b034e9506a7e
mhans003 <michaeledwardhanson@gmail.com>
2020-11-02 18:25:27 -0800

    test selectAll async/await

--- models/todo.js
+++ models/todo.js
@@ -1,9 +1,9 @@
 const orm = require("../config/orm"); 
 
 const todo = {
-    selectAll: function(callBack) {
+    selectAll: async function(callBack) {
         orm.selectAll("todos", function(result) {
-            callBack(result); 
+            await callBack(result); 
         }); 
     },
     insertOne: function(columns, values, callBack) {
