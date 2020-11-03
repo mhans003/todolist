88aba544c5e61d2e72686ec55d5ffb38cd97a412
mhans003 <michaeledwardhanson@gmail.com>
2020-11-02 17:25:59 -0800

    Imported credentials to env variables

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,18 @@ const mysql = require("mysql");
 //Define connection. 
 const connection = mysql.createConnection({
   //host: "localhost",
-  host: "us-cdbr-east-02.cleardb.com",
+  //host: "us-cdbr-east-02.cleardb.com",
+  host: process.env.DB_HOST,
   //port: 3306,
   //user: "root",
-  user: "bc63f9baf3dc33",
+  //user: "bc63f9baf3dc33",
+  user: process.env.DB_USER,
   //password: "",
-  password: "b8ceb667",
+  //password: "b8ceb667",
+  password: process.env.DB_PASSWORD,
   //database: "todolist_db"
-  database: "heroku_46a814c5751ed29"
+  //database: "heroku_46a814c5751ed29"
+  database: process.env.DB_DATABASE
 });
 
 //Establish connection. 
